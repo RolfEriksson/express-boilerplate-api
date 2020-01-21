530061c99a8cf2d4d0035bf54c0688bf67c6e01a
Rolf Eriksson <rolf.eriksson.m@gmail.com>
2020-01-21 14:18:34 -0600

    Added env variables for username, password, host

--- src/api/v1/models/index.ts
+++ src/api/v1/models/index.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 import User from "./User";
 
 const connectDb = () => {
-    return mongoose.connect(process.env.DATABASE_URL);
+    return mongoose.connect(
+      `mongodb://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}`
+    );
 };
 
 const models = { User };
