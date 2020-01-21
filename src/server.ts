782fc18e9f0451167f458429e49250d094e77196
Rolf Eriksson <rolf.eriksson.m@gmail.com>
2020-01-20 22:45:56 -0600

    added develop script.

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,5 @@ const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => res.send("Hello world!"));
 
-app.listen(port);
+// tslint:disable-next-line: no-console
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
