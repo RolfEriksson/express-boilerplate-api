cf3e5d74f7d7893a9070808f612cb2d4b56e9358
Rolf Eriksson <rolf.eriksson.m@gmail.com>
2020-01-21 13:02:45 -0600

    Added dummy error handling for auth.

--- src/api/v1/controllers/validateCredentials.ts
+++ src/api/v1/controllers/validateCredentials.ts
@@ -1,7 +1,16 @@
 import { Errback, Request, Response } from "express";
 import { ErrorHandler } from "../helpers/error";
 
-const getToken = async () => "TOKEN_FOOO__BARRRR";
+const getToken = async (username: string, password: string) => {
+  try {
+    if (!(username === "rolf.eriksson" && password === "password1234")) {
+      throw new Error("INCORRECT_CREDENTIALS");
+    }
+    return "TOKENASDASDASD";
+  } catch (error) {
+    throw new ErrorHandler(400, error.message);
+  }
+};
 
 const validateCredentials = async (req: Request, res: Response, next: Errback) => {
   try {
@@ -9,7 +18,7 @@ const validateCredentials = async (req: Request, res: Response, next: Errback) =
     if (!username || !password) {
       throw new ErrorHandler(422, "Missing required username and password fields");
     }
-    const token = await getToken();
+    const token = await getToken(username, password);
     res.send({
       token
     });
