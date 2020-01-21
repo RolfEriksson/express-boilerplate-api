1d700cb26d0f483ed436200b9673280b71f68ad2
Rolf Eriksson <rolf.eriksson.m@gmail.com>
2020-01-21 10:42:54 -0600

    Added redirect to documentation.

--- src/api/v1/index.ts
+++ src/api/v1/index.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import swaggerUI from "swagger-ui-express";
+
+import movieRouter from "./routes/movieRouter";
+
 import { swaggerSpecV1 } from "../../swaggerDocGenerator";
 
 const api = express();
 
-api.use("/", swaggerUI.serve, swaggerUI.setup(swaggerSpecV1));
+api.get("/", (req, res) => res.redirect("api-docs"));
+api.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpecV1));
+api.use("/movies", movieRouter);
 
 export default api;
