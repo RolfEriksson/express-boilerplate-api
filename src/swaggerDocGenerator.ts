e1eb7e903390826458aaf1b3b5f9afd23fb566a6
Rolf Eriksson <rolf.eriksson.m@gmail.com>
2020-01-22 16:10:08 -0600

    Adjusted options for swagger config.

--- src/swaggerDocGenerator.ts
+++ src/swaggerDocGenerator.ts
@@ -1,13 +1,32 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
 const optionsV1 = {
-    apis: ["./api/v1"],
-    definition: {
-        info: {
-            title: "express-boilerplate-api",
-            version: "1.0.0"
-        }
+  apis: ["src/api/v1/routes/*Router.ts"],
+  swaggerDefinition: {
+    info: {
+        contact: {
+            email: "rolf.eriksson.m@gmail.com",
+            name: "Rolf Eriksson"
+        },
+        description:
+            "A simple boilerplate for bootstrapping an Express + Typescript + Mongo API.",
+        license: {
+            name: "MIT",
+            url: "https://choosealicense.com/licenses/mit/"
+        },
+        title: "express-boilerplate-api",
+        version: "1.0.0"
     },
+    openapi: "3.0.0",
+    servers: [
+      {
+        url: "http://localhost:3000/api/v1"
+      },
+      {
+          url: "https://express-boilerplate-api.herokuapp.com/api/v1"
+      }
+    ]
+  }
 };
 
 export const swaggerSpecV1 = swaggerJSDoc(optionsV1);
